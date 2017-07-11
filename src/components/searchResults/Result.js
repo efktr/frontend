8ae9de664b40d81dc1bee9ee5ff887d0b80cc0d2
Christian Dallago <code@dallago.us>
2017-07-11 16:34:51 +0200

    small steps

--- src/components/searchResults/Result.js
+++ src/components/searchResults/Result.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react';
 import {ListItem} from 'material-ui/List';
 import {Link} from 'react-router-dom';
 import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
+
 
 const warningIcon = <FontIcon className="material-icons">warning</FontIcon>;
 const drugIcon = <FontIcon className="material-icons">blur_circular</FontIcon>;
+
+const addIcon = <FontIcon className="material-icons">add</FontIcon>;
+const removeIcon = <FontIcon className="material-icons">remove</FontIcon>;
+
 
 
 export default class Result extends Component {
@@ -24,7 +30,14 @@ export default class Result extends Component {
                 leftIcon={(this.state.element.type === "adr"? warningIcon : drugIcon)}
                 primaryText={this.state.element.name}
                 secondaryText={"Search matched by a " + this.state.element.type}
+                // Implement if-else logic + storage in redux
+                rightToggle={
+                    <IconButton>
+                        {addIcon}
+                    </IconButton>
+                }
             />
+
         </Link>)
     };
 }
