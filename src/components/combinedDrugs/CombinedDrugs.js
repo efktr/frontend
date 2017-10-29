f8ee422177825ddd4e20c7f770791f29e6e7fd0d
Christian Dallago <code@dallago.us>
2017-10-29 18:21:38 +0100

    merging and displaying

--- src/components/combinedDrugs/CombinedDrugs.js
+++ src/components/combinedDrugs/CombinedDrugs.js
@@ -1,40 +1,70 @@
 import React, {Component} from 'react';
 import {get} from 'jquery';
-import { __API__, __DRUG__, __ADRS__, buildUrl } from './../../globals';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import { __API__, __ADR__, __ADRS__, buildUrl } from './../../globals';
+import PossibleAdverseDrugReactionItem from '../drug/PossibleAdverseDrugReactionItem';
 import {List} from 'material-ui/List';
 import CircularProgress from 'material-ui/CircularProgress';
 import {GridList, GridTile} from 'material-ui/GridList';
 import AdverseDrugReactionItem from '../drug/AdverseDrugReactionItem';
-import '../../utilities/prototypeExtensions'
+import '../../utilities/prototypeExtensions';
+import Range from '../../utilities/Range';
+import PubSub from 'pubsub-js';
+
 
 export default class Drug extends Component {
 
     constructor(props) {
-
         super(props);
-
-        // Implement GET ADRs for combination of drugs
-
-        // this.fetchAdrs();
-
 
         this.state = {
             drugs: [],
             adrs: [],
-            adrsLoaded: false
+            displayed: [],
+            adrsLoaded: false,
+            items: 10,
+            inputValue: '',
+            searchAdrs: []
         };
+
+        this.handleScroll = this.handleScroll.bind(this);
+        this.searchAdrs = this.searchAdrs.bind(this);
+    }
+
+    searchAdrs() {
+        const url = buildUrl([__API__, __ADR__ + "?q=" + this.state.inputValue]);
+        get(url, (adrs) => {
+            this.setState({
+                searchAdrs: adrs.filter(e => !this.state.adrs.find(i => i.umls_id === e.umlsid))
+            });
+        }).fail(() => {
+            console.log("Failed to load ADRs");
+        });
+    }
+
+    handleScroll() {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+        const windowBottom = windowHeight + window.pageYOffset;
+        if (windowBottom >= (docHeight - 10)) {
+            this.setState({
+                items: (this.state.items + 10 > this.state.displayed.length ? this.state.displayed.length : this.state.items + 10)
+            });
+        }
     }
 
     componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+
         let handleDrugEvent = () => {
             let drugs = this.props.store.getState().drugs;
-
-            console.log(drugs);
 
             this.setState({
                 drugs: drugs
             });
+
+            this.fetchAdrs(drugs);
         };
 
         this.storeSubscription = this.props.store.subscribe(handleDrugEvent);
@@ -42,18 +72,40 @@ export default class Drug extends Component {
         this.props.store.dispatch({
             type: "GET_DRUGS"
         });
+
+        this.subscription = PubSub.subscribe('SEARCH_INPUT_CHANGE', (message, data) => {
+            this.setState({
+                inputValue: data
+            }, () => {
+                this.filter();
+                this.searchAdrs();
+            });
+        });
+    }
+
+    filter() {
+        this.setState({
+            displayed: this.state.adrs.filter((element) => {return element.name.match(new RegExp(this.state.inputValue, "i"))})
+        })
     }
 
     componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+
         // Will unsubscribe from store sub
         this.storeSubscription();
+
+        PubSub.unsubscribe(this.subscription);
     }
 
-    fetchAdrs() {
-        const url = buildUrl([__API__, __ADRS__, this.state.drugbankId]);
+    fetchAdrs(drugs) {
+        let queryString = "?q=" + drugs.map(e => e.identifier).join("&q=");
+
+        const url = buildUrl([__API__, __ADRS__ + queryString]);
         get(url, (adrs) => {
             this.setState({
                 adrs: adrs,
+                displayed: adrs,
                 adrsLoaded: true
             });
         }).fail(() => {
@@ -74,6 +126,35 @@ export default class Drug extends Component {
     }
 
     render() {
+        let filteredAndSorted = this.state.displayed
+            .map(e => {
+                if(e => e.lower !== null && e.higher !== null){
+                    e.range = new Range(e.lower, e.higher);
+                }
+                return e;
+            })
+            .sort((e,i) => {
+                //return (e.lower === null || e.higher === null) && (i.lower !== null && i.higher !== null)
+                //return e.range !== undefined && i.range === undefined
+                return i.lower > e.lower && i.higher > e.higher
+            })
+            .slice(0, this.state.items);
+
+        let suggestions = <div/>;
+
+        if(this.state.searchAdrs.length > 0){
+            suggestions = <div>
+                <p className="drugsContainer">
+                    <strong>
+                        This effects match your search and may not be reported by this drug (click + to add them):
+                    </strong>
+                </p>
+                <List className="possibleAndrsContainer">
+                    {this.state.searchAdrs.map(e => <PossibleAdverseDrugReactionItem key={e.umlsid} data={e}/>)}
+                </List>
+            </div>;
+        }
+
         return (<div>
                 <GridList cols={3}>
                     {this.state.drugs.map((drug) => (
@@ -87,10 +168,10 @@ export default class Drug extends Component {
                     ))}
                 </GridList>
                 {this.loading()}
-                <List>
-                    {this.state.adrs.map(e => <AdverseDrugReactionItem key={e.name} data={e}/>)}
+                <List className="drugsContainer">
+                    {filteredAndSorted.map(e => <AdverseDrugReactionItem key={e.umls_id} data={e}/>)}
                 </List>
-
+                {suggestions}
             </div>
         )
     };
